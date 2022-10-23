7829a1e9779507006c2ff5e79f23e14d72d56e0b
Alison Golden <alisong779@yahoo.com>
2022-10-23 18:42:10 -0400

    add showEditState to UI

--- app.js
+++ app.js
@@ -149,6 +149,7 @@ const UICtrl = (function(){
         addItemToForm: function(){
             document.querySelector(UISelectors.itemNameInput).value = ItemCtrl.getCurrentItem().name;
             document.querySelector(UISelectors.itemCaloriesInput).value = ItemCtrl.getCurrentItem().calories;
+            UICtrl.showEditState();
         },
 
         hideList: function(){
@@ -165,6 +166,14 @@ const UICtrl = (function(){
             document.querySelector(UISelectors.deleteBtn).style.display = 'none';
             document.querySelector(UISelectors.backBtn).style.display = 'none';
             document.querySelector(UISelectors.addBtn).style.display = 'inline';
+
+        },
+
+        showEditState: function(){
+            document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+            document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+            document.querySelector(UISelectors.backBtn).style.display = 'inline';
+            document.querySelector(UISelectors.addBtn).style.display = 'none';
 
         },
  
